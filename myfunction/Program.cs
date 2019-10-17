d8cb881d9996bb1e47f2713c590847a99ea33a2f
Opilane TTHK <opilane@tthk.ee>
2019-10-17 17:09:09 +0300

    next

---
+++ myfunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace myfunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! What's your name?");
+            string usersName = Console.ReadLine();
+            Console.WriteLine("What's your favorite color?");
+            string userfavColor = Console.ReadLine();
+            KnowMe(usersName, userfavColor);
+        }
+        public static void KnowMe (string name, string color)
+        {
+            Console.WriteLine($"Hello, {name}. Your favorite color is {color}. ");
+
+        }
+    }
+}
